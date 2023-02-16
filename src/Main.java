4d6c1559bf269af29162821cf968a0a62c71b0af
Edson <edsonluizn.silva@gmail.com>
2023-02-16 14:35:10 -0300

    Added MainProgram

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        //NumberGenerator numgen = new NumberGenerator();
+        AnotherMethod();
+    }
+    static void AnotherMethod(){
+        System.out.println("Another method");
+    }
+}
